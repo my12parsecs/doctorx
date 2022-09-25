a88d76c4ad9aa33da22c1c76318bf99976106599
HikaruImai <hikaru@HikaruImainoMacBook-Pro.local>
2022-09-26 01:55:21 +0900

    hashrouter

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 
+import "./index.css";
+import App from "./App";
 
-import './index.css';
-import App from './App';
-
-ReactDOM.render((
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-        <App />
-    </BrowserRouter>
-), document.getElementById("root"))
+ReactDOM.render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
