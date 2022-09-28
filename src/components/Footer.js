b962370104c32380c7f48215320d3722a2deddf8
HikaruImai <hikaru@HikaruImainoMacBook-Pro.local>
2022-09-29 06:53:51 +0900

    added contact form link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,30 +1,40 @@
-import { t } from "i18next"
-import { Link } from "react-router-dom"
+import { t } from "i18next";
+import { Link } from "react-router-dom";
 
-export default function Footer(){
-    return(
-        <footer className="footer">
-            {/* <img></img> */}
-            <h3 className="hospital">東帝大学病院</h3>
-            <div className="footer-split">
-                <div className="footer-left">
-                    <div className="footer-about">
-                        <h3>{t("aboutSite")}</h3>
-                        <p>{t("aboutSiteExplanation")}</p>
-                        <a href="https://www.tv-asahi.co.jp/doctor-x/">{t("officialSite")}</a>
-                    </div>
-                    <a href="" className="footer-twitter">@DoctorXFan</a>
-                </div>
-                <div className="footer-page">
-                    <Link>エピソード</Link>
-                    <h3>hello</h3>
-                </div>
-            </div>
-            <div className="footer-bottom">
-                <li>{t("aboutSite")}</li>
-                <li>{t("sitemap")}</li>
-                <li>{t("contact")}</li>
-            </div>
-        </footer>
-    )
+export default function Footer() {
+  return (
+    <footer className="footer">
+      {/* <img></img> */}
+      <h3 className="hospital">東帝大学病院</h3>
+      <div className="footer-split">
+        <div className="footer-left">
+          <div className="footer-about">
+            <h3>{t("aboutSite")}</h3>
+            <p>{t("aboutSiteExplanation")}</p>
+            <a href="https://www.tv-asahi.co.jp/doctor-x/">
+              {t("officialSite")}
+            </a>
+          </div>
+          <a href="" className="footer-twitter">
+            @DoctorXFan
+          </a>
+        </div>
+        <div className="footer-page">
+          <Link>エピソード</Link>
+          <h3>hello</h3>
+        </div>
+      </div>
+      <div className="footer-bottom">
+        <a>{t("aboutSite")}</a>
+        <a>{t("sitemap")}</a>
+        <a
+          href="https://docs.google.com/forms/d/e/1FAIpQLScoU6_8uAFqiih3tZiwMoU1TvXo3LVn2PDy4qFqglZDzISNrg/viewform"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {t("contact")}
+        </a>
+      </div>
+    </footer>
+  );
 }
