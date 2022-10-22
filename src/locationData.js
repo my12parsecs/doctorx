807a2af3f2de38d27ba44eb7b56309963d049e04
HikaruImai <imai.hikaru@gmail.com>
2022-10-22 21:16:01 +0900

    started location page

---
+++ src/locationData.js
@@ -0,0 +1,24 @@
+import i18next, { t } from "i18next";
+
+const locationData = [
+    {
+        id: "1",
+        season: "",
+        scene: "",
+        image: "",
+        location: "",
+        address: "",
+        explanation: ""
+    },
+    {
+        id: "1",
+        season: "",
+        scene: "",
+        image: "",
+        location: "",
+        address: "",
+        explanation: ""
+    }
+]
+
+export default locationData
--- src/pages/Location.js
+++ src/pages/Location.js
@@ -1,6 +1,23 @@
-
 export default function Location(){
+
+    // これからやること => episodeやcharacterのようにlocationDataからmapする
+
     return(
-        <div>location</div>
+        <div className="location">
+            <article className="location-article">
+                <div className="location-title">
+                    <h3 className="location-season">シーズン1~5</h3>
+                    <h2 className="location-name">神原名医紹介所</h2>
+                </div>
+                <div className="location-info">
+                    <img className="location-image"></img>
+                    <div className="location-detail">
+                        <h4 className="location-detail1">コスガ</h4>
+                        <h4 className="location-detail2">日本東京都江東区佐賀1丁目</h4>
+                        <p className="location-detail3">ほにゃらら</p>
+                    </div>
+                </div>
+            </article>
+        </div>
     )
 }
