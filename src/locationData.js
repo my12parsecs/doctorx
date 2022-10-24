3821e6fc308d33474c8486d22973d65239b09bdd
HikaruImai <imai.hikaru@gmail.com>
2022-10-24 11:36:58 +0900

    added

--- src/locationData.js
+++ src/locationData.js
@@ -3,11 +3,11 @@ import i18next, { t } from "i18next";
 const locationData = [
     {
         id: "1",
-        season: "",
-        scene: "",
+        season: "1~5",
+        scene: "神原名医紹介所",
         image: "",
-        location: "",
-        address: "",
+        location: "コスガ",
+        address: "日本東京都江東区佐賀1丁目",
         explanation: ""
     },
     {
